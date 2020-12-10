9a32c30b62a5be4686de8b2bff53c3f84c078c72
paulotrujillo <paulo_trujillo@hotmail.com>
2020-12-09 23:39:40 -0600

    21: Múltiples exportaciones e importacionesp/defecto

---
+++ 02-intro-javascript/src/bases/08-imp-exp.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import heroes, { owners } from '../data/heroes';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+const getHeroeById = (id) => heroes.find( (heroe) => heroe.id === id);
+
+const getHeroeByName = (name) => heroes.find( (heroe) => heroe.name === name );
+
+const getHeroesByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner );
+
+
+console.log(owners);
+console.log( getHeroeById(4) );
+console.log( getHeroeByName('Gualguarin') );
+console.log( getHeroesByOwner('Marvel') );
+
