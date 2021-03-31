d9bb18712b479d951892f045ea835860cf4cb75a
paulotrujillo <paulo_trujillo@hotmail.com>
2021-03-31 14:42:58 -0600

    35: Mi 1er componente de React
    Creando un componente super sencillo de React :)

---
+++ 03-counter-app/src/PrimeraApp.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//FunctionalComponents
+const PrimeraApp = () => {
+    return <h1>Zenlex Component</h1>
+}
+export default PrimeraApp;
+
+
--- 03-counter-app/src/index.js
+++ 03-counter-app/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PrimeraApp from './PrimeraApp';
+import './index.css';
 
-const saludo = <h1>Zenlex</h1>;
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render(saludo, divRoot);
+ReactDOM.render(<PrimeraApp />  ,divRoot);
 console.log(divRoot);
